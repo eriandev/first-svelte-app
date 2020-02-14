a8b4236a23b95345ee8cac2cfeb949496d4fe094
erianvc <erianvc.dev@gmail.com>
2020-02-13 21:09:22 -0500

    First Svelte App completed

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,41 @@
 <script>
-	export let name;
+	export let API, missingName, missingImg;
+    
+    let pokeName = 'POKEMON';
+    let pokeImg = 'images/pokeball.png';
+
+    function getData(stringToFind){
+
+        return fetch(`${API}${stringToFind}`).then(res => res.json());
+    }
+
+    async function handleSubmit(event){
+
+        const textValue = event.target.pokeName.value.trim().toLowerCase();
+        try {
+            const data = await getData(textValue);
+            pokeName = data.name.toUpperCase();
+            pokeImg = data.sprites.front_default;
+        } 
+        catch (e) {
+            pokeName = missingName;
+            pokeImg = missingImg;
+        } 
+        finally {
+            document.getElementById('pokeForm').reset();
+        }
+    }
 </script>
 
-<main class="flex flex-col max-w-lg p-4 my-0 mx-auto">
-	<h1 class="text-6xl text-red-500 font-hairline uppercase">Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-</style>
+<div class="flex flex-wrap bg-gray-900 h-screen content-center text-center">
+    <div class="w-full">
+        <form id="pokeForm" on:submit|preventDefault={handleSubmit}>
+            <span class="text-md font-bold text-white">{pokeName}</span>
+            <img class="mx-auto" src={pokeImg} alt={pokeName}>
+            <input id="pokeName" class="bg-gray-200 hover:bg-white focus:bg-white py-2 px-4 my-2 text-gray-700 leading-tight border border-transparent hover:border-gray-300 focus:border-gray-300 focus:outline-none focus:shadow-outline appearance-none rounded" type="text" placeholder="Nombre del pokemon" autocomplete="off" required>
+            <button class="bg-blue-500 hover:bg-blue-400 py-2 px-4 text-white font-bold border-b-4 border-blue-700 hover:border-blue-500 focus:outline-none rounded">
+                Buscar
+            </button>
+        </form>
+    </div>
+</div>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import App from './App.svelte';
 
+const API = 'https://pokeapi.co/api/v2/pokemon/';
+const missingName = 'MissingNa';
+const missingImg = 'images/missingno.png';
+
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'mundo'
-	}
+    target: document.body,
+    props: {
+        API,
+        missingName,
+        missingImg,
+    }
 });
 
 export default app;
